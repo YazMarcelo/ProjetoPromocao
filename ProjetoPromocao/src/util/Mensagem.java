cb9a5bc2d610040e9517bd65fe43c05397ca0bd2
Marcelo <marcellohugo01@gmail.com>
2017-11-28 10:55:04 -0200

    Criação da classe de mensagens; Correções no cadastro de produto;

--- ProjetoPromocao/src/util/Mensagem.java
+++ ProjetoPromocao/src/util/Mensagem.java
@@ -5,6 +5,7 @@
  */
 package util;
 
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -12,49 +13,63 @@ import javax.swing.JOptionPane;
  * @author HELM
  */
 public class Mensagem {
-    
+
     /*
     icone de mensagens
     0 = vermelho
     1 = azul de informação
     2 = Amarelo de Atenção
     3 = cinza de questão
-    */
-    
-    public void msg01(){
-        JOptionPane.showMessageDialog(null, "Cadastro Efetuado com Sucesso!", "Mensagem do Sistema", 1);
+     */
+    public static void msg01() {
+        JOptionPane.showMessageDialog(null, "Registro Efetuado Com Sucesso!", "Mensagem do Sistema", 1);
     }
-    public int msg02(){
-        return JOptionPane.showConfirmDialog(null, "Confirmar Alteração?", "Mensagem do Sistema", 0, 3);
+
+    public static int msg02() {
+        return JOptionPane.showConfirmDialog(null, "Confirmar Alterações?", "Mensagem do Sistema", 0, 3);
     }
-    public int msg03(){
+
+    public static int msg03() {
         return JOptionPane.showConfirmDialog(null, "Confirmar Exclusão?", "Mensagem do Sistema", 0, 3);
     }
-    public void msg04(){
+
+    public static void msg04() {
         JOptionPane.showMessageDialog(null, "Alteração Efetuada com Sucesso!", "Mensagem do Sistema", 1);
     }
-    public void msg05(){
+
+    public static void msg05() {
         JOptionPane.showMessageDialog(null, "Exclusão Efetuada com Sucesso!", "Mensagem do Sistema", 1);
     }
-    public void msg06(){
+
+    public static void msg06() {
         JOptionPane.showMessageDialog(null, "Dado Inválido!", "Mensagem do Sistema", 0);
     }
-    public void msg07(){
-        JOptionPane.showMessageDialog(null, "Perfil Inválido! Verifique os dados digitados.", "Mensagem do Sistema", 0);
+
+    public static void msg07() {
+        JOptionPane.showMessageDialog(null, "Registro Informado está cancelado!", "Mensagem do Sistema", 1);
     }
-    public void msg08(){
-        JOptionPane.showMessageDialog(null, "Registro buscado não está cadastrado no sistema ou os dados estão incorretos! Por favor, verifique!", "Mensagem do Sistema", 0);
+
+    public static void msg08() {
+        JOptionPane.showMessageDialog(null, "Não foi possível acessar a base de dados, tente novamente.", "Mensagem do Sistema", 0);
     }
-    public void msg09(){
+
+    public static void msg09() {
         JOptionPane.showMessageDialog(null, "Registro já existente!", "Mensagem do Sistema", 0);
     }
-    public void msg10(){
-        JOptionPane.showMessageDialog(null, "CPF Inválido!", "Mensagem do Sistema", 0);
+
+    public static void msg10() {
+        JOptionPane.showMessageDialog(null, "Informe os dados obrigatórios!", "Mensagem do Sistema", 0);
     }
-    public void msg11(){
-        JOptionPane.showMessageDialog(null, "Informe os dados Obrigatórios!", "Mensagem do Sistema", 1);
+
+    public static void msg11() {
+        JOptionPane.showMessageDialog(null, "Lista de promoção enviada com Sucesso!", "Mensagem do Sistema", 1);
     }
-    public void msg12(){
-        JOptionPane.showMessageDialog(null, "Selecione um Registro!", "Mensagem do Sistema", 1);
+
+    public static void msg12() {
+        JOptionPane.showMessageDialog(null, "Pedido Faturado com Sucesso!", "Mensagem do Sistema", 1);
+    }
+
+    public static void msg13() {
+        JOptionPane.showMessageDialog(null, "Pedido Cancelado com Sucesso!", "Mensagem do Sistema", 1);
     }
 }
